dddc4b788c67b66894309270f8929425ed882b0b
OlehKotov <mail.oleg456@gmail.com>
2024-02-24 21:07:36 +0200

    Update bs-books.js

--- src/js/bs-books.js
+++ src/js/bs-books.js
@@ -91,94 +91,64 @@ SeeMoreBtn.addEventListener("click", async function (event) {
  
   try {
     refs.bestBooksSection.innerHTML = '';
-    const data = await getCategoryBooks(category);
-    renderMarkup(data);
-     console.log(data);
+    const array = await getCategoryBooks(category);
+    console.log(array);
+    renderMarkup(array);
       } catch (error) {
-        console.error(error.message);
+        // console.error(error.message);
       }
 });
 
-function booksCategoryTemplate({
-  _id,
-  book_image,
-  title,
-  author,
-    }) {
+function createCategoryItem(data) {
+  return `
+  <h1 class="bs-books-title">
+    Best Sellers <span class="bs-books-title-blue">Books</span>
+  </h1> <ul class="bs-list list"> ${data
+    .map(elements => {
       return `
-              <a href="#" class="bs-books-item-link link" rel="noopener noreferrer" data-id='${_id}'>
-        
-              <div class="bs-books-card">
-                <img
-                  src="${book_image}"
-                  alt="${title}"
-                  class="bs-books-card-img"
-                  width="180"
-                  height="256"
-                  loading="lazy"
-                />
+    <li class="bs-books-item"> 
+    <h3 class="bs-books-category-title">${elements.list_name}</h3>
+      <div class="bs-books-thumb" data-list-id="${elements.list_name}">
+        ${elements.books
+          .map(book => {
+            return `
 
-               </div> 
-                <div class="bs-books-item-thumb">
-                  <h3 class="bs-books-item-title">${title.slice(0, 18)}</h3>
-                  <p class="bs-books-author">${author}</p>
-                </div>
-             </a>`;
-    }
+          <a href="#" class="bs-books-item-link link" rel="noopener noreferrer" data-id='${book._id}'>
+    
+          <div class="bs-books-card">
+            <img
+              src="${book.book_image}"
+              alt="${book.title}"
+              class="bs-books-card-img"
+              width="180"
+              height="256"
+              loading="lazy"
+            />
+
+           </div> 
+            <div class="bs-books-item-thumb">
+              <h3 class="bs-books-item-title">${book.title.slice(0, 18)}</h3>
+              <p class="bs-books-author">${book.author}</p>
+            </div>
+         </a>`;
+          })
+          .join('')}
+      </div>
+      <div class="bs-books-thumb-btn">
+      <button class="bs-books-see-more-btn" type="button" data-id="${
+        elements.list_name
+      }">see more</button>
+      </div>
+      
+    </li>
+    `;
+    })
+    .join('')}</ul>`;
+    
+}
+
+
     function renderMarkup(books) {
-          const markup = books.map(booksCategoryTemplate).join('');
+          const markup = booksCategoryTemplate(books);
           refs.bestBooksSection.insertAdjacentHTML('beforeend', markup);
         }
-
-
-
-
-
-
-
-
-
-
-// async function handleButtonClick(e) {
-//   const id = await e.target.dataset.id;
-//   // createCategoryMarkup(id);
-//   console.log("Hello");
-// }
-// const parent = document.querySelector(".bs-books-item");
-
-// parent.addEventListener("click", (event) => {
-//   console.log("event.target: ", event.target);
-//   console.log("event.currentTarget: ", event.currentTarget);
-// });
-
-
-
-// async function onSeeMoreBtn(e) {
-//   if (e.target.nodeName !== 'BUTTON') return;
-//   const query = e.target.closest('button').name;
-  
-//   try {
-//     refs.bestBooksSection.innerHTML = '';
-//     const getCategory = await getCategoryBooks(query);
-//     const paintedList = await paintMarkup(caughtCategory, query);
-//     bookListRef.innerHTML = paintedList;
-    
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-
-//   
-  
-//   let gallery = new SimpleLightbox('.gallery a', {
-//     showCounter: false,
-//     captionDelay: 250,
-//     captions: true,
-//     captionsData: 'alt',
-//     captionPosition: 'bottom',
-//   });
-  
-//   function renderMarkup(images) {
-//     const markup = images.map(galleryTemplate).join('');
-//     refs.gallery.insertAdjacentHTML('beforeend', markup);
-//     gallery.refresh();
-//   }
