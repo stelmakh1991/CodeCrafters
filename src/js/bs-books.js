bebd75558c4db1e32e2479e39dec69a28c61d56d
OlehKotov <mail.oleg456@gmail.com>
2024-02-25 09:40:59 +0200

    Update

--- src/js/bs-books.js
+++ src/js/bs-books.js
@@ -48,15 +48,12 @@ async function createGalleryItem(data) {
                   src="${book.book_image}"
                   alt="${book.title}"
                   class="bs-books-card-img"
-                  width="180"
-                  height="256"
-                  loading="lazy"
                 />
 
                </div> 
                 <div class="bs-books-item-thumb">
                   <h3 class="bs-books-item-title">${book.title.slice(0, 18)}</h3>
-                  <p class="bs-books-author">${book.author}</p>
+                  <p class="bs-books-author">${book.author.slice(0, 18)}</p>
                 </div>
              </a>`;
               })
@@ -93,62 +90,33 @@ SeeMoreBtn.addEventListener("click", async function (event) {
     refs.bestBooksSection.innerHTML = '';
     const array = await getCategoryBooks(category);
     console.log(array);
-    renderMarkup(array);
+    createCategoryItem(array);
       } catch (error) {
-        // console.error(error.message);
+        console.error(error.message);
       }
 });
 
-function createCategoryItem(data) {
-  return `
-  <h1 class="bs-books-title">
-    Best Sellers <span class="bs-books-title-blue">Books</span>
-  </h1> <ul class="bs-list list"> ${data
-    .map(elements => {
-      return `
-    <li class="bs-books-item"> 
-    <h3 class="bs-books-category-title">${elements.list_name}</h3>
-      <div class="bs-books-thumb" data-list-id="${elements.list_name}">
-        ${elements.books
-          .map(book => {
-            return `
-
-          <a href="#" class="bs-books-item-link link" rel="noopener noreferrer" data-id='${book._id}'>
-    
-          <div class="bs-books-card">
-            <img
-              src="${book.book_image}"
-              alt="${book.title}"
-              class="bs-books-card-img"
-              width="180"
-              height="256"
-              loading="lazy"
-            />
-
-           </div> 
-            <div class="bs-books-item-thumb">
-              <h3 class="bs-books-item-title">${book.title.slice(0, 18)}</h3>
-              <p class="bs-books-author">${book.author}</p>
-            </div>
-         </a>`;
-          })
-          .join('')}
+function createCategoryItem(books) {
+  let arr = books;
+  let bookCategoryTitle = `<h1 class="bs-category-title">${books[0].list_name}</h1>`;
+  let bookCategoryList = '<ul class="bs-category-list list">';
+  arr.forEach(({_id, book_image, title, author, list_name}) => {
+    bookCategoryList += `<li class="bs-category-item" id="${_id}">
+    <div class="bs-books-thumb" data-list-id="${list_name}">
+    <a href="#" class="bs-books-item-link link" rel="noopener noreferrer" data-id='${_id}'>
+    <div class="bs-category-books-card">  
+      <img src="${book_image}" alt="${title}" class="bs-books-card-img"/>
       </div>
-      <div class="bs-books-thumb-btn">
-      <button class="bs-books-see-more-btn" type="button" data-id="${
-        elements.list_name
-      }">see more</button>
+      <div class="bs-books-item-thumb">
+      <h3 class="bs-books-item-title">${title.slice(0, 18)}</h3>
+      <p class="bs-books-author">${author.slice(0, 18)}</p>
       </div>
+      </a>
+      </li>
       
-    </li>
-    `;
-    })
-    .join('')}</ul>`;
-    
+      </div>`;
+  });
+  bookCategoryList += '</ul>';
+  bookCategoryTitle += bookCategoryList
+  refs.bestBooksSection.insertAdjacentHTML('beforeend', bookCategoryTitle);
 }
-
-
-    function renderMarkup(books) {
-          const markup = booksCategoryTemplate(books);
-          refs.bestBooksSection.insertAdjacentHTML('beforeend', markup);
-        }
