7247d447a6a636d5eb6ff3bcec676cb349d1fb38
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-02-26 08:33:41 +0200

    import bs-books.css  update general styles

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { BooksApi } from './js/books-api';
+import { renderBooksList } from './js/render-main-page';
+import { renderTopBooks } from './js/render-main-page';
+import { renderCategory } from './js/render-main-page';
+import { renderBook } from './js/render-main-page';
+import { onGalleryItemClick } from './js/render-main-page';
+import { onShowCategoryBtnClick } from './js/render-main-page';
+import { onAddAndRemoveToShoppingListOnModal } from './js/render-main-page';
+import { renderShoppingList } from './js/render-main-page';
+import { renderBookFromLocalStorage } from './js/render-main-page';
+import { onRemoveFromShoppingList } from './js/render-main-page';
